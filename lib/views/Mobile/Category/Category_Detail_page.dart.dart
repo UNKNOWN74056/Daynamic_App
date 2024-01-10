b317f16989ca5c9c023f4ac8cc997709f0682beb
UNKNOWN74056 <hamza74056@gmail.com>
2024-01-10 12:17:51 +0500

    changes

--- lib/views/Mobile/Category/Category_Detail_page.dart.dart
+++ lib/views/Mobile/Category/Category_Detail_page.dart.dart
@@ -7,16 +7,16 @@ import 'package:flutter/material.dart';
 import 'package:flutter_gutter/flutter_gutter.dart';
 
 class Category_details_page extends StatefulWidget {
+  final Items_data selectedItem;
+  final List<Items_data> itemList;
+  final Stores store;
+
   const Category_details_page({
     super.key,
     required this.selectedItem,
     required this.itemList,
     required this.store,
   });
-
-  final Items_data selectedItem;
-  final List<Items_data> itemList;
-  final Stores store;
 
   @override
   State<Category_details_page> createState() => _Category_details_pageState();
@@ -26,7 +26,7 @@ class _Category_details_pageState extends State<Category_details_page> {
   @override
   Widget build(BuildContext context) {
     final selectedItem = widget.selectedItem;
-    final itemList = widget.itemList; // Access itemList from the widget
+    final itemList = widget.itemList;
 
     return Scaffold(
       appBar: AppBar(
@@ -56,14 +56,12 @@ class _Category_details_pageState extends State<Category_details_page> {
             ),
           ),
           const Gutter(),
-          // Now you can use itemList to display products based on your logic
           Expanded(
             child: ListView.builder(
               itemCount: itemList.length,
               itemBuilder: (context, index) {
                 final product = itemList[index];
 
-                // Check if the product's itemType matches the selected item's itemType
                 if (product.itemType == selectedItem.itemType) {
                   return GestureDetector(
                     onTap: () {
@@ -72,23 +70,20 @@ class _Category_details_pageState extends State<Category_details_page> {
                         MaterialPageRoute(
                           builder: (context) => Details_page(
                             item: itemList[index],
-                            store: widget.store, // Pass the selected item
+                            store: widget.store,
                           ),
                         ),
                       );
                     },
-                    child: Expanded(
-                      child: ProductWidget(
-                        discount: product.itemDiscount ?? "",
-                        currency: product.itemCurrency ?? "",
-                        imagePath: imagespath + (product.itemPhoto1 ?? ""),
-                        productName: product.itemName ?? "",
-                        productPrice: product.itemPrice ?? "",
-                      ),
+                    child: ProductWidget(
+                      discount: product.itemDiscount ?? "",
+                      currency: product.itemCurrency ?? "",
+                      imagePath: imagespath + (product.itemPhoto1 ?? ""),
+                      productName: product.itemName ?? "",
+                      productPrice: product.itemPrice ?? "",
                     ),
                   );
                 } else {
-                  // Return an empty container if the itemType doesn't match
                   return Container();
                 }
               },
